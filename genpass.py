87fd11b01b72060bafe43d4c3ba3b84535a1a766
BeWhoYouWantToBe <286537741@qq.com>
2017-10-31 14:26:53 +0800

    update

--- genpass.py
+++ genpass.py
@@ -7,17 +7,17 @@ def genpass():
     domain = args.domain
     username = args.username
 
-    rules = open('./rules/rules.txt')
     name = domain[0] if domain else username[0]
+    rules = open('./rules/domain_rules.txt') if domain else open('./rules/username_rules.txt')
     pwds = open('{}.txt'.format(name),'w')
 
     for rule in rules:
         if username:
-            pwd = rule.replace('%name%',username[0])
+            pwd = rule.replace('%username%',username[0])
             pwds.write(pwd) 
-            pwds.write(pwd.capitalize())
+            pwds.write(pwd.capitalize()) 
         if domain:
-            pwd = rule.replace('%name%',domain[0]) 
+            pwd = rule.replace('%domain%',domain[0]) 
             pwds.write(pwd)
             pwds.write(pwd.capitalize()) 
 
